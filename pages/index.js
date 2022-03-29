199caf42a7f829d3661e45167226283b98e1cdaa
Simon Vrachliotis <simon@simonswiss.com>
2022-03-29 13:06:18 +1100

    ship large image for twitter homepage card

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,14 @@ import PageHeading from '/components/page-heading'
 import GridList from '/components/grid-list'
 import PageMeta from '/components/page-meta'
 
+import image from '/public/images/social-large.jpg'
+
 import { getPosts } from '/lib/getData'
 
 const meta = {
   title: 'Helping developers create better screencasts',
   description: `A collection of resources to help developers create better screencasts, and level-up their overall video content creation game.`,
+  image,
 }
 
 const data = getPosts()
