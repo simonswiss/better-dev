5ea6f68b4a859d7efed51497bd2b6f8a2b2a5450
Simon Vrachliotis <simon@simonswiss.com>
2023-03-09 17:02:03 +1100

    Keystatic footer ⚡️

--- components/footer.js
+++ components/footer.js
@@ -4,6 +4,10 @@ export default function Footer() {
       <div className="mx-auto mt-20 mb-10 max-w-7xl px-4 md:px-6 lg:mt-32 lg:px-8 2xl:max-w-8xl">
         <p className="text-center">
           Copyright &copy; {new Date().getFullYear()} Better Dev Screencasts. Built with{' '}
+          <a className="underline  hover:text-indigo-500" href="https://keystatic.thinkmill.com.au">
+            Keystatic
+          </a>
+          ,{' '}
           <a className="underline  hover:text-indigo-500" href="https://nextjs.org">
             Next.js
           </a>{' '}
